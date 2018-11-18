c13677bcdea85f1efa327aeb06ae8ca9e7c22ab0
jeffreyh93 <jeffreyhuynh0@gmail.com>
2018-11-18 00:20:58 -0800

    Finished minesweeper and added executable jar files

--- Minesweeper/MinesweeperScreen.java
+++ Minesweeper/MinesweeperScreen.java
@@ -10,11 +10,13 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -39,8 +41,12 @@ public class MinesweeperScreen extends Application {
     private int numCols = 8;
     private int numMines = 10;
     private int numClicks = 0;
+    private Node[][] gridPaneNode;
+    private boolean[][] revealArr;
     StackPane rootPane;
     private boolean[][] gameArray;
+    private int[][] logicArray;
+
     /**main game window of the game.
      * @param args
      */
@@ -64,29 +70,56 @@ public class MinesweeperScreen extends Application {
             //FlowPane flow = new FlowPane();
             for (int j = 0; j < numRows; j++) {
                 Button temp = new Button();
-                temp.setMinWidth(30);
-                temp.setMinHeight(30);
+                temp.setMinWidth(40);
+                temp.setMinHeight(40);
                 root.add(temp, i, j);
             }
         }
-        contents();
+        logicArray = new int[numRows][numCols];
+
+        processLogic();
+        //contents();
+        revealArr = new boolean[numRows][numCols];
+        gridPaneNode = new Node[numRows][numCols];
+        for (Node myNodeAdd : root.getChildren()) {
+            int myCol = GridPane.getColumnIndex(myNodeAdd);
+            int myRow = GridPane.getRowIndex(myNodeAdd);
+            gridPaneNode[myRow][myCol] = myNodeAdd;
+            revealArr[myRow][myCol] = false;
+        }
         
         for (Node element : root.getChildren()) {
             element.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-
                 @Override
                 public void handle(MouseEvent event) {
                     // TODO Auto-generated method stub
                     int currRow = GridPane.getRowIndex(element);
                     int currCol = GridPane.getColumnIndex(element);
+                    
                     if(event.isPrimaryButtonDown()) {
-                        // check mines here
-                        if(numClicks == 0) {
-                            
+                        // process left mouse clicks
+                        if (logicArray[currRow][currCol] == 0) {
+                            // reveal all zero adjacent cells until non-zero is found
+                            Button myB = (Button)gridPaneNode[currRow][currCol];
+                            myB.setText("" + logicArray[currRow][currCol]);
+                            revealArr[currRow][currCol] = true;
+                            revealAdjZero(currRow, currCol);
+                        } else if (logicArray[currRow][currCol] == -1) {
+                            // lost game, reveal all numbers
+                            for (Node element1 : root.getChildren()) {
+                                int rowRev = GridPane.getRowIndex(element1);
+                                int colRev = GridPane.getColumnIndex(element1);
+                                Button bReveal = (Button)element1;
+                                bReveal.setText("" + logicArray[rowRev][colRev]);
+                            }
+                        } else {
+                            Button b = (Button)element;
+                            b.setText("" + logicArray[currRow][currCol]);
                         }
-                        numClicks++;
                     } else if (event.isSecondaryButtonDown()) {
                         // add red flags here
+                        Button setFlag = (Button)element;
+                        setFlag.setStyle("-fx-background-color: red");
                     }                    
                 }
             });
@@ -95,6 +128,224 @@ public class MinesweeperScreen extends Application {
         primaryStage.setTitle("Minesweeper");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    public void revealAdjZero(int myRow, int myCol) {
+        if (logicArray[myRow][myCol] == 0) {
+            if (myRow == 0 && myCol == 0) {
+                //top left corner
+                for (int i = 0; i < 2; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            }
+            else if (myRow == 0 && myCol == numCols - 1) {
+                // top right corner
+                for (int i = 0; i < 2; i++) {
+                    for (int j = -1; j < 1; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            }
+            else if (myRow == numRows - 1 && myCol == 0) {
+                // bottom left corner
+                for (int i = -1; i < 1; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            }
+            else if (myRow == numRows - 1 && myCol == numCols - 1) {
+                // bottom right corner
+                for (int i = -1; i < 1; i++) {
+                    for (int j = -1; j < 1; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            } else if (myRow == 0) {
+                // we're on the top edge
+                for (int i = 0; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            } else if (myCol == 0) {
+                // we're on the left edge
+                for (int i = -1; i < 2; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            } else if (myRow == numRows - 1) {
+                // we're on the bottom edge 
+                for (int i = -1; i < 1; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            } else if (myCol == numCols - 1) {
+                // we're on the right edge
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 1; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            } else {
+                // middle
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (!revealArr[myRow + i][myCol + j]) {
+                            Button revButton = (Button)gridPaneNode[myRow + i][myCol + j];
+                            revButton.setText("" + logicArray[myRow + i][myCol + j]);
+                            revealArr[myRow + i][myCol + j] = true;
+                            revealAdjZero(myRow + i, myCol + j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // Used to assign numerical values given the mines
+    public void processLogic() {
+        
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (gameArray[i][j]) {
+                    // simplest case, there is a mine there so assign a value of -1
+                    logicArray[i][j] = -1;
+                } else {
+                    logicArray[i][j] = checkAdj(i, j);
+                }
+            }
+        }
+    }
+    public int checkAdj(int row, int col) {
+        int bombsCount = 0;
+        if (row == 0 && col == 0) {
+            // top left corner
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else if (row == 0 && col == numCols - 1) {
+            // top right corner
+            for (int i = 0; i < 2; i++) {
+                for (int j = -1; j < 1; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else if (row == numRows - 1 && col == 0) {
+            // bottom left corner
+            for (int i = -1; i < 1; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else if (row == numRows - 1 && col == numCols - 1) {
+            // bottom right corner
+            for (int i = -1; i < 1; i++) {
+                for (int j = -1; j < 1; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else if (row == 0) {
+            // we're on the top edge
+            for (int i = 0; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else if (col == 0) {
+            // we're on the left edge
+            for (int i = -1; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else if (row == numRows - 1) {
+            // we're on the bottom edge 
+            for (int i = -1; i < 1; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else if (col == numCols - 1) {
+            // we're on the right edge
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 1; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        } else {
+            // we're somewhere in the middle
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (gameArray[row + i][col + j]) {
+                        bombsCount++;
+                    }
+                }
+            }
+        }
+        return bombsCount;
     }
     /** Check if the array list contains a mine or not.
      * 
@@ -109,7 +360,7 @@ public class MinesweeperScreen extends Application {
         for (int i = 0; i < numRows; i++) {
             System.out.println("Row " + (i + 1) + ":");
             for (int j = 0; j < numCols; j++) {
-                System.out.print(gameArray[i][j] + "\t");
+                System.out.print(logicArray[i][j] + "\t");
             }
             System.out.println("\n");
         }
