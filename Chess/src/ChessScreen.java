5df409539fdc317b2b8b3be9ba584aed1157de0c
jeffreyh93 <jeffreyhuynh0@gmail.com>
2018-12-26 20:35:15 -0800

    Finished setting up the pieces and displaying a grey and white chessboard

--- Chess/src/ChessScreen.java
+++ Chess/src/ChessScreen.java
@@ -9,7 +9,16 @@ import javafx.stage.Stage;
  *
  */
 public class ChessScreen extends Application {
-    
+    /** 3rd row of the chessboard. */
+    private final int thirdR = 3;
+    /** 4th row of the chessboard. */
+    private final int fourthR = 4;
+    /** 5th row of the chessboard. */
+    private final int fifthR = 5;
+    /** 6th row of the chessboard. */
+    private final int sixthR = 6;
+    /** 7th row of the chessboard. */
+    private final int seventhR = 7;
     /** Sets the rows for the chessboard. */
     private final int numRows = 8;
     /** Sets the cols for the chessboard. */
@@ -45,11 +54,6 @@ public class ChessScreen extends Application {
                 Button temp = new Button();
                 temp.setMinWidth(buttonSize);
                 temp.setMinHeight(buttonSize);
-                //if (j == 1) {
-                //    temp.setStyle("-fx-background-image: "
-                //            + "url('img/whitePawn.png')");
-                    
-                //}
                 String bgColor = "";
                 if (j == 1) {
                     if (i % 2 == 0) {
@@ -58,10 +62,12 @@ public class ChessScreen extends Application {
                         bgColor = "white";
                     }
                     temp.setStyle("-fx-background-color: " + bgColor + ";"
-                            + "-fx-background-image: url('img/whitePawn1.png');"
+                            + "-fx-background-image: url('img/whitePawn.png');"
                             + "-fx-background-size: 100px 100px;");
-                }
-                else if (j == 6) {
+                    root.add(temp, i, j);
+                } else if (j == 0) {
+                    setPieces(true);
+                } else if (j == sixthR) {
                     if (i % 2 == 0) {
                         bgColor = "white";
                     } else {
@@ -70,28 +76,71 @@ public class ChessScreen extends Application {
                     temp.setStyle("-fx-background-color: " + bgColor + ";"
                             + "-fx-background-image: url('img/blackPawn.png');"
                             + "-fx-background-size: 100px 100px;");
-                }
-                else if (i % 2 == 0) {
+                    root.add(temp, i, j);
+                } else if (j == seventhR) {
+                    setPieces(false);
+                } else if (i % 2 == 0) {
                     if (j % 2 == 0) {
                         temp.setStyle("-fx-background-color: white;");
                     } else {
                         temp.setStyle("-fx-background-color: grey");
                     }
+                    root.add(temp, i, j);
                 } else {
                     if (j % 2 == 0) {
                         temp.setStyle("-fx-background-color: grey");
                     } else {
                         temp.setStyle("-fx-background-color: white");
-                    }   
+                    }
+                    root.add(temp, i, j);
                 }
-                
-                root.add(temp, i, j);
             }
         }
-        setPieces();
     }
-    /** Sets the background images to the corresponding pieces.*/
-    private void setPieces() {
+    /** Sets the background images to the corresponding pieces.
+     *  @param firstRow checks whether it is the first or last row
+     */
+    private void setPieces(boolean firstRow) {
+        String bgColor = "";
+        String pColor = "";
+        String pName = "";
+        int currRow = 0;
+        if (firstRow) {
+            bgColor = "white";
+            pColor = "white";
+            currRow = 0;
+        } else {
+            bgColor = "grey";
+            pColor = "black";
+            currRow = seventhR;
+        }
+        for (int i = 0; i < numCols; i++) {
+            Button temp = new Button();
+            temp.setMinWidth(buttonSize);
+            temp.setMinHeight(buttonSize);
+            if (i == 0 || i == seventhR) {
+                pName = "Rook";
+            } else if (i == 1 || i == sixthR) {
+                pName = "Knight";
+            } else if (i == 2 || i == fifthR) {
+                pName = "Bishop";
+            } else if (i == thirdR) {
+                pName = "Queen";
+            } else if (i == fourthR) {
+                pName = "King";
+            }
+            temp.setStyle("-fx-background-color: " + bgColor + ";"
+                    + "-fx-background-image: url('img/" 
+                    + pColor + pName + ".png');"
+                    + "-fx-background-size: 100px 100px;");
+            root.add(temp, i, currRow);
+            if (bgColor.equals("white")) {
+                bgColor = "grey";
+            } else {
+                bgColor = "white";
+            }
+            
+        }
         
     }
 }
